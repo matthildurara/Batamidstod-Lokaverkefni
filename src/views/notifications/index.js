3ad1b34e6bc18890e48c8a176858bbcac51fc46a
Matthildur Aradóttir <matthildur@Matthildurs-MacBook-Pro.local>
2022-05-03 10:24:14 +0000

    comment

--- src/views/notifications/index.js
+++ src/views/notifications/index.js
@@ -18,44 +18,7 @@ const NotificationView = ({ navigation, route }) => {
   const parameter = route.params.toolbarText;
   const db = getDatabase();
   const dbRef = ref(db, "Users/Notifications");
-  //const [allNotifications, setAllNotifications] = useState([]);
   const [allNotifications, setAllNotifications] = useState([]);
-
-  // const fetchNotifications = async () => {
-  //   onValue(dbRef, (snapshot) => {
-  //     //setAllNotifications([]);
-  //     console.log("=============MMMMM==========");
-  //     snapshot.forEach((childSnapshot) => {
-  //       const childKey = childSnapshot.key;
-  //       console.log("CHILDKEU IS ?");
-  //       console.log(childKey);
-  //       childSnapshot.forEach((childChild) => {
-  //         const childchildKey = childChild.key;
-  //         console.log("childchild key: ", childchildKey);
-  //         const childValue = childChild.val();
-
-  //         const item = {
-  //           notification: childValue.notification,
-  //           notificationTitle: childValue.notificationTitle,
-  //         };
-
-  //         console.log("ITEM IS: ", item);
-  //         // setAllNotifications((prevState) => ({
-  //         //   ...prevState,
-  //         //   [childSnapshot.key]: item,
-  //         // }));
-  //         setAllNotifications((allNotifications) => [
-  //           ...allNotifications,
-  //           item,
-  //         ]);
-  //         let newArray = [...allNotifications];
-  //         newArray.slice().reverse();
-  //         console.log("NEEEW ARRAY: ", newArray);
-  //         setAllNotifications(newArray);
-  //       });
-  //     });
-  //   });
-  // };
 
   useEffect(() => {
     const db = getDatabase();
@@ -91,43 +54,6 @@ const NotificationView = ({ navigation, route }) => {
     //console.log(allNotifications);
   }, []);
 
-  // useEffect(() => {
-  //   const fetchNotifications = async () => {
-  //     onValue(dbRef, (snapshot) => {
-  //       setAllNotifications([]);
-  //       snapshot.forEach((childSnapshot) => {
-  //         const childKey = childSnapshot.key;
-  //         const childVal = childSnapshot.val();
-  //         allNotifications[childKey] = [];
-  //         const item = {
-  //           Title: childVal.notificationTitle,
-  //           Notification: childVal.notification,
-  //         };
-  //         setAllNotifications((allNotifications) => [
-  //           ...allNotifications,
-  //           item,
-  //         ]);
-  //       });
-  //     });
-  //   };
-  //   fetchNotifications();
-  //   return;
-  // }, []);
-  // console.log("ALL NOT");
-  // console.log(Object.values(allNotifications));
-
-  // const renderItem = (item) => {
-  //   //console.log(item);
-  //   //console.log(dayValue);
-  //   return (
-  //     // {item.date==}
-  //     <View style={styles.notificationContainer}>
-  //       <Text>{item.notificationTitle}</Text>
-  //       <Text>{item.notification}</Text>
-  //     </View>
-  //   );
-  // };
-  //console.log(allNotifications);
   return (
     <View style={styles.container}>
       <Toolbar toolbarText={parameter} style={styles.toolbar} />
